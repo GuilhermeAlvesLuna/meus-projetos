92174a7731b92c119ea09da6f02a8808aadba053
GuilhermeAlvesLuna <161092170+GuilhermeAlvesLuna@users.noreply.github.com>
2024-12-30 12:17:55 -0300

    Update index.js

--- brat-2/index.js
+++ brat-2/index.js
@@ -1,4 +1,3 @@
-// Função para alternar as cores do fundo e do texto
 function mudarCor() {
     const h1 = document.getElementById('brat');  // Seleciona o h1 pelo id
     const link = h1.querySelector('a');  // Seleciona o link dentro do h1
@@ -8,10 +7,12 @@ function mudarCor() {
     // Usamos setInterval para alternar as cores a cada 1 segundo
     setInterval(function() {
         if (alternaCor) {
+            // Cor de fundo verde e texto preto (incluindo o link)
             h1.style.backgroundColor = "#89CC04";  // Cor de fundo verde
             h1.style.color = "black";  // Cor do texto preta
             link.style.color = "black";  // Cor do link preta
         } else {
+            // Cor de fundo preta e texto verde (incluindo o link)
             h1.style.backgroundColor = "black";  // Cor de fundo preta
             h1.style.color = "#89CC04";  // Cor do texto verde
             link.style.color = "#89CC04";  // Cor do link verde
@@ -23,6 +24,7 @@ function mudarCor() {
 
 // Chama a função assim que a página estiver carregada
 window.onload = mudarCor;
+
 
 /*
 function mudarCor(){
