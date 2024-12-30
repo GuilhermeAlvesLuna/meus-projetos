719843e0feba5b8653b72fff96e285093df7793e
GuilhermeAlvesLuna <161092170+GuilhermeAlvesLuna@users.noreply.github.com>
2024-12-30 10:52:28 -0300

    atualização

--- brat-2/index.js
+++ brat-2/index.js
@@ -1,11 +1,13 @@
-for(i=0; i<10; i++){
-    setInterval(function mudarCor(){
-        h1.style.backgroundColor = "black";
-        h1.style.color = "#89CC04";
-    }, 1000);
-
-    setInterval(function mudarCorNovamente(){
-        h1.style.backgroundColor = "#89CC04";
-        h1.style.color = "black";
-    }, 1000);
+function mudarCor(){
+    for(i=0; i<10; i++){
+        setInterval(function() {
+            h1.style.backgroundColor = "black";
+            h1.style.color = "#89CC04";
+        }, 1000);
+    
+        setInterval(function(){
+            h1.style.backgroundColor = "#89CC04";
+            h1.style.color = "black";
+        }, 1000);
+    }
 }
